7000c84641a9df369694395096561fb5f9d24058
plem99 <1730314@upv.edu.mx>
2020-05-22 10:20:39 -0500

    update

--- Unidad_1/Practica_1/Models/conexion2.php
+++ Unidad_1/Practica_1/Models/conexion2.php
@@ -1,7 +1,6 @@
 <?php
-    class Conexion{
 
-        public function conectar(){
+
             //$link = new PDO("mysql:host=localhost;dbname=datos", "root", "");
             try{
                 $link = new PDO("mysql:host=localhost;dbname=practica1", "root", "");
@@ -11,7 +10,7 @@
             }
             return $link;
 
-        }
-    }
+
+
 
 ?>
