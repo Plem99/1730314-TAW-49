be89a63bbfc1c81a1e3fe513f94111cba63020df
plem99 <1730314@upv.edu.mx>
2020-05-26 09:13:59 -0500

    update

--- Unidad_1/Practica_25052020_Inventario/Models/crud.php
+++ Unidad_1/Practica_25052020_Inventario/Models/crud.php
@@ -25,6 +25,34 @@
             return $stmt->fetchAll();
             $stmt->close();
         }
+
+        /*-- Este modelo sirve para insertar un nuevo usuario a la bd --*/
+        public function insertarUserModel($datosModel,$tabla) {
+            //preparamos el PDO
+            $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla (firstname,lastname,user_name,user_password,user_email) VALUES (:nusuario,:ausuario,:usuario,:contra,:email)");
+            $stmt->bindParam(":nusuario",$datosModel["nusuario"],PDO::PARAM_STR);
+            $stmt->bindParam(":ausuario",$datosModel["ausuario"],PDO::PARAM_STR);
+            $stmt->bindParam(":usuario",$datosModel["usuario"],PDO::PARAM_STR);
+            $stmt->bindParam(":contra",$datosModel["contra"],PDO::PARAM_STR);
+            $stmt->bindParam(":email",$datosModel["email"],PDO::PARAM_STR);
+            if ($stmt->execute()) {
+                return "success";
+            } else {
+                return "error";
+            }
+            $stmt->close();
+        }
+
+        /*-- Este modelo sirve para cargar la información del usuario para su posterior modificación --*/
+        public function editarUserModel($datosModel, $tabla) {
+           
+            $stmt = Conexion::conectar()->prepare("SELECT user_id AS 'id', firstname AS 'nusuario', lastname AS 'ausuario', user_name AS 'usuario', user_password AS 'contra', user_email AS 'email' FROM $tabla WHERE user_id=:id");
+            
+            $stmt->bindParam(":id",$datosModel,PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch();
+            $stmt->close();
+        }
         
 
         //******************************************************************************/
