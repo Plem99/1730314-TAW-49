087a0cb931ffd2052cf7a4cbd880bd5e65ae05ad
plem99 <1730314@upv.edu.mx>
2020-06-01 07:35:36 -0500

    update

--- Unidad_1/Practica_25052020_Inventario/bd.sql
+++ Unidad_1/Practica_25052020_Inventario/bd.sql
@@ -1,46 +1,44 @@
 /*Creacion de base de datos*/
-CREATE DATABASE practica1;
-/*Utilizando la base de datos practica1*/
-USE practica1;
-/*Tabla para el usuario*/
-CREATE TABLE tusuario(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(45),
-    contrasena VARCHAR(45),
-    email VARCHAR(45)
-);
-/*Tabla para la empresa*/
-CREATE TABLE tempresa(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(45)
-);
+CREATE DATABASE simple_stock;
+/*Utilizando la base de datos simple_stock*/
+USE simple_stock;
 /*Tabla para las categorias*/
-CREATE TABLE tcategoria(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(45)
+CREATE TABLE categories(
+    id_category INT AUTO_INCREMENT PRIMARY KEY,
+    name_category VARCHAR(255),
+    description_category VARCHAR(255),
+    date_added datetime
 );
-/*Tabla para el proveedor*/
-CREATE TABLE tproveedor(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    clave VARCHAR(45),
-    nombre VARCHAR(45),
-    rfc VARCHAR(45),
-    email VARCHAR(45),
-    id_empresa INT,
-    id_categoria INT,
-    FOREIGN KEY (id_empresa) REFERENCES tempresa (id),
-    FOREIGN KEY (id_categoria) REFERENCES tcategoria (id)
+/*Tabla para los usuarios*/
+CREATE TABLE users(
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(20),
+    lastname VARCHAR(20),
+    user_name VARCHAR(64),
+    user_password VARCHAR(255),
+    user_email VARCHAR(64),
+    date_added datetime
 );
-/*-------------------------*/
+/*Tabla para los productos*/
+CREATE TABLE products(
+    id_product INT AUTO_INCREMENT PRIMARY KEY,
+    code_producto char(20),
+    name_product char(255),
+    date_added datetime,
+    price_product float(8,2),
+    stock INT,
+    id_category INT,
+    FOREIGN KEY (id_category) REFERENCES categories (id_category)
+);
 /*Tabla para el historial*/
-CREATE TABLE thistorial(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    clave VARCHAR(45),
+CREATE TABLE historial(
+    id_historial INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(45),
-    rfc VARCHAR(45),
-    email VARCHAR(45),
-    id_empresa INT,
-    id_categoria INT,
-    FOREIGN KEY (id_empresa) REFERENCES tempresa (id),
-    FOREIGN KEY (id_categoria) REFERENCES tcategoria (id)
+    date datetime,
+    note VARCHAR(255),
+    reference VARCHAR(100),
+    id_producto INT,
+    user_id INT,
+    FOREIGN KEY (id_producto) REFERENCES products (id_product),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
