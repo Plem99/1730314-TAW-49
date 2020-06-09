2645822d368c85f3dcfb715020c84001a4cb0179
plem99 <1730314@upv.edu.mx>
2020-06-09 07:50:42 -0500

    update

--- Unidad_1/Practica_25052020_Inventario/Models/conexion.php
+++ Unidad_1/Practica_25052020_Inventario/Models/conexion.php
@@ -4,7 +4,7 @@
         public function conectar(){
             //$link = new PDO("mysql:host=localhost;dbname=datos", "root", "");
             try{
-                $link = new PDO("mysql:host=localhost:8080;dbname=simple_stock", "root", "");
+                $link = new PDO("mysql:host=localhost;dbname=simple_stock", "root", "");
             }catch (PDOException $e){
                 print "¡Error!: " . $e->getMessage() . "<br/>";
                 die();
